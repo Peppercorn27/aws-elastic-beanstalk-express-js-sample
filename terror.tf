de1e080a724b824c4a4cffcce31e1e3ea238f9b9
Peppercorn27 <desmond2027@gmail.com>
2021-06-21 10:45:55 +0100

    update

--- terror.tf
+++ terror.tf
@@ -1,10 +1,16 @@
 # create beanstalk webapp
 
 provider "aws" {
-  region                =    "eu-west-2"
+  region                =   "eu-west-2"
 }
 
-resource "aws_elastic_beanstalk_application" "DevOpsBean1" {
-  name                  = "DevOpsGettingStarted"
-  solution_stack_name   = "Node.js 12 AL2 version 5.1.0"
+resource "aws_elastic_beanstalk_application" "DevOpsTest" {
+  name                  =   "DevOpsGettingStarted"
+  description           =   "DevOpsGettingStarted"
+}
+
+resource "aws_elastic_beanstalk_environment" "DevOpsTest" {
+  name                  =   "DevOpsGettingStarted"
+  application           =   aws_elastic_beanstalk_application.DevOpsTest.name
+  solution_stack_name   =   "64bit Amazon Linux 2 v5.4.1 running Node.js 12"
 }
