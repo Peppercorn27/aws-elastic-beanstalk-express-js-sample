c0685bec910c6d3325d1a3f64f596dc7e09b4f9a
Peppercorn27 <desmond2027@gmail.com>
2021-06-21 10:24:54 +0100

    Update terror.tf

--- terror.tf
+++ terror.tf
@@ -1,6 +1,10 @@
 # create beanstalk webapp
 
+provider "aws" {
+  region                =    "eu-west-2"
+}
+
 resource "aws_elastic_beanstalk_application" "DevOpsBean1" {
-  name          = "DevOpsGettingStarted"
-  solution_stack_name  = "Node.js 12 AL2 version 5.1.0"
+  name                  = "DevOpsGettingStarted"
+  solution_stack_name   = "Node.js 12 AL2 version 5.1.0"
 }
